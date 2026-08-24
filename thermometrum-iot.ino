fdd3775c99ba81a726e9d5c3026af5253ca46eec
CLEVER <sanya.veselkov.811@gmail.com>
2026-08-24 22:36:06 +0600

    feat: main ino

---
+++ thermometrum-iot.ino
@@ -0,0 +1,110 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+#include <DHT.h>
+
+// config
+const char* SSID         = "wifi name";
+const char* PASSWORD     = "wifi pass";
+const char* MQTT_BROKER  = "192.168.0.17";
+const int   MQTT_PORT    = 1883;
+
+const char* DEVICE_ID    = "thermometrum-node-03";
+const uint64_t SLEEP_TIME = 60000000; // 1 min
+
+
+
+#define DHTPIN 4
+#define DHTTYPE DHT22
+
+DHT dht(DHTPIN, DHTTYPE);
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+
+  Serial.begin(115200);
+  delay(500);
+  Serial.println("\nstart");
+
+  dht.begin();
+
+  float temperature = dht.readTemperature();
+  float humidity = dht.readHumidity();
+
+  if (isnan(temperature) || isnan(humidity)) {
+    Serial.println("[ERROR] reading DHT22 data error, going to sleep");
+    goToSleep();
+  }
+
+  if (temperature < -90.0 || temperature > 90.0 || humidity < 0.0 || humidity > 100.0) {
+    Serial.println("[ERROR] value(s) is(are) not in allowed interval, you lowkey shoulda check your sensor");
+    goToSleep();
+  }
+
+  Serial.printf("[LOCAL] T: %.2f °C | H: %.2f %%\n", temperature, humidity);
+
+
+
+  Serial.print("[WIFI] connecting to ");
+  Serial.println(SSID);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(SSID, PASSWORD);
+
+  int attempt = 0;
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    attempt++;
+    if (attempt > 30) {
+      Serial.println("\n[ERROR] unable to connect to WIFI, going to sleep");
+      goToSleep();
+    }
+  }
+  Serial.println("\n[LOG] connected, local IP: " + WiFi.localIP().toString());
+
+
+
+  client.setServer(MQTT_BROKER, MQTT_PORT);
+  Serial.print("[LOG] connecting to MQTT broker");
+
+  if (client.connect(DEVICE_ID)) {
+    char topic[64];
+    snprintf(topic, sizeof(topic), "thermometrum/%s/reading", DEVICE_ID);
+
+    JsonDocument doc;
+    doc["temperature"] = round(temperature * 100) / 100.0;
+    doc["humidity"]    = round(humidity * 100) / 100.0;
+
+    char payload[128];
+    size_t n = serializeJson(doc, payload);
+
+    if (client.publish(topic, (const uint8_t*)payload, n, false)) {
+      Serial.print("[LOG] sent: ");
+      Serial.println(payload);
+    } else {
+      Serial.println("[ERROR] unable to publish MQTT message");
+    }
+
+    client.disconnect();
+  } else {
+    Serial.print("[ERROR] error occured while using MQTT, status: ");
+    Serial.println(client.state());
+  }
+
+
+
+  goToSleep();
+}
+
+void loop() {
+  // useless
+}
+
+void goToSleep() {
+  Serial.println("[LOG] take a nap");
+  digitalWrite(LED_BUILTIN, HIGH);
+  ESP.deepSleep(SLEEP_TIME);
+}
